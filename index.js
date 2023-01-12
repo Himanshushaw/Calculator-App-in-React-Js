0504ea5d06da32cc74dea288b5fde2bc189d12ab
Himanshu Shaw <65149117+Himanshushaw@users.noreply.github.com>
2023-01-12 18:06:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 
 
 ReactDOM.render(
-   
-        <h1>Hello world</h1>,
-        
-            
-        
-    document.getElementById("root")
-);
+   <App />,
+    document.getElementById('root')
+)
 
